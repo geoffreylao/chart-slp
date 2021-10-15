a85fe6c7e0919b8c6a0858b87d4bb1db7bca4568
geoffrey lao <geoffreylao@outlook.com>
2021-10-15 20:12:53 -0300

    font family

--- react-slip/src/http-common.js
+++ react-slip/src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://chart-slp-server.herokuapp.com/api/",
-  //baseURL: "http://localhost:8080/api",
+  //baseURL: "https://chart-slp-server.herokuapp.com/api/",
+  baseURL: "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json"
   }
