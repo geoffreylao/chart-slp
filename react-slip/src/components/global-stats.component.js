c8165de0e7d8bbaa14fcdd3f888311bc1d995343
geoffrey lao <geoffreylao@outlook.com>
2022-03-20 19:56:52 -0300

    updated global stats

--- react-slip/src/components/global-stats.component.js
+++ react-slip/src/components/global-stats.component.js
@@ -828,7 +828,7 @@ export default class MatchStats extends Component {
 
         <div className="row">
           <div className="col-lg-12">
-            <h3 id="searchParams">Global Snapshot made on 2022-01-01 at 505041 Games and 45304 Unique Players</h3>
+            <h3 id="searchParams">Global Snapshot made on 2022-03-20 at 845247 Games and 55553 Unique Players</h3>
           </div>  
         </div>
 
