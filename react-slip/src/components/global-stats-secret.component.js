ec1ec45fa5f1d1fac3e83ee4a74a04ce84239a11
geoffreylao <geoffreylao@outlook.com>
2023-06-04 17:21:45 -0300

    end db maitenance

--- react-slip/src/components/global-stats-secret.component.js
+++ react-slip/src/components/global-stats-secret.component.js
@@ -1,6 +1,8 @@
-import React, { Component, useState } from 'react';
+import React, { Component, 
+  //useState 
+} from 'react';
 import Select from 'react-select';
-import Alert from 'react-bootstrap/Alert';
+//import Alert from 'react-bootstrap/Alert';
 
 import PieChart from './charts/pie-chart.component';
 import CharBarChart from './charts/char-bar-chart.component';
@@ -836,22 +838,22 @@ function createCharColorBarChart(charId, charColorsArr, title) {
   );
 }
 
-function AlertDismissible() {
-  const [show, setShow] = useState(true);
+// function AlertDismissible() {
+//   const [show, setShow] = useState(true);
 
-  return (
-    <Alert
-      variant='secondary'
-      show={show}
-      onClose={() => setShow(false)}
-      dismissible
-    >
-      <p className='alert'>
-        Database is down for maitenance! Sorry for the inconvenience!
-      </p>
-    </Alert>
-  );
-}
+//   return (
+//     <Alert
+//       variant='secondary'
+//       show={show}
+//       onClose={() => setShow(false)}
+//       dismissible
+//     >
+//       <p className='alert'>
+//         Database is down for maitenance! Sorry for the inconvenience!
+//       </p>
+//     </Alert>
+//   );
+// }
 
 export default class Secret extends Component {
   constructor(props) {
@@ -1236,7 +1238,7 @@ export default class Secret extends Component {
 
     return (
       <div className='container mt-3'>
-        <AlertDismissible/>
+        {/* <AlertDismissible/> */}
         <div className='list row'>
           <div className='col-md-12'>
             <Container fluid>            
